59c58c424f98a6b6b9cefefa8b9cd3dfe0c38b17
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-23 14:29:07 +0100

    Remove username from json

--- internal/database/models/meal.go
+++ internal/database/models/meal.go
@@ -8,7 +8,7 @@ type Meal struct {
 	ID          int         `json:"id" db:"id" binding:"-"`
 	Name        string      `json:"name" db:"name" binding:"required"`
 	Timestamp   time.Time   `json:"timestamp" db:"timestamp" binding:"required"`
-	Username    string      `json:"username" db:"username" binding:"-"`
+	Username    string      `json:"-" db:"username" binding:"-"`
 	CreatedAt   time.Time   `json:"created_at" db:"created_at" binding:"-"`
 	MealEntries []MealEntry `json:"meal_entries" db:"-"`
 }
